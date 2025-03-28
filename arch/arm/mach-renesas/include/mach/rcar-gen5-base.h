bd130731e3e4e79b28164257606d78ff1016a536
Hai Pham <hai.pham.ud@renesas.com>
2025-03-28 12:29:21 +0700

    ARM: renesas: Add R8A78000 X5H Kconfig entry and PRR ID
    Add Kconfig entry and PRR ID to support R8A78000 X5H SoC.
    
    Signed-off-by: Hai Pham <hai.pham.ud@renesas.com>

---
+++ arch/arm/mach-renesas/include/mach/rcar-gen5-base.h
@@ -0,0 +1,40 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * ./arch/arm/mach-renesas/include/mach/rcar-gen5-base.h
+ *
+ * Copyright (C) 2023 Renesas Electronics Corp.
+ */
+
+#ifndef __ASM_ARCH_RCAR_GEN5_BASE_H
+#define __ASM_ARCH_RCAR_GEN5_BASE_H
+
+#define TMU_BASE	0x1C030000
+
+/* Arm Generic Timer */
+/* TODO: No information for R-Car Gen5, to be updated */
+#define CNTCR_BASE      0xE6080000
+#define CNTFID0         (CNTCR_BASE + 0x020)
+#define CNTCR_EN        BIT(0)
+
+/* GICv4 */
+/* Distributor Registers */
+#define GICD_BASE	0x38000000
+#define GICR_BASE       (GICR_LPI_BASE)
+
+/* ReDistributor Registers for Control and Physical LPIs */
+#define GICR_LPI_BASE	0x38080000
+#define GICR_WAKER	0x0014
+#define GICR_PWRR	0x0024
+#define GICR_LPI_WAKER	(GICR_LPI_BASE + GICR_WAKER)
+#define GICR_LPI_PWRR	(GICR_LPI_BASE + GICR_PWRR)
+
+/* ReDistributor Registers for SGIs and PPIs */
+#define GICR_SGI_BASE	0x38090000
+#define GICR_IGROUPR0	0x0080
+
+#ifndef __ASSEMBLY__
+#include <asm/types.h>
+#include <linux/bitops.h>
+#endif
+
+#endif /* __ASM_ARCH_RCAR_GEN5_BASE_H */
